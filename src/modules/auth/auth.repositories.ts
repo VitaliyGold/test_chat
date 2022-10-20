540a3a4386e94824c364b5d75a00b33ed33a37c8
Vitaliy Prokhorov <kysko68@yandex.ru>
2022-10-21 00:57:46 +0300

    add login methods

--- src/modules/auth/auth.repositories.ts
+++ src/modules/auth/auth.repositories.ts
@@ -1,22 +1,37 @@
 import { PostgresDb } from '@fastify/postgres';
-import { RegistrationModel } from '../../types/auth';
+import { RegistrationModel, UserModel } from '../../types/auth';
+import { QueryResult } from 'pg';
 
-export async function getUserToLogin(bd: PostgresDb, user_login: string):  {
+export async function checkUseLogin(bd: PostgresDb, user_login: string): Promise<boolean> {
     const { rows } = await bd.query(
-        `SELECT login FROM auth_data WHERE login=$1`, [user_login]
-    )
+        `SELECT login FROM auth_data WHERE login=$1 LIMIT 1`, [user_login]
+    );
     if (rows.length) {
-        return rows
+        return true;
     }
-    return null
+    return false;
+}
+
+export async function getUserData(bd: PostgresDb, user_login: string): Promise<UserModel | null> {
+    const { rows }: QueryResult<UserModel> = await bd.query(
+        `SELECT auth_data.login, auth_data.user_id, auth_data.password, user_data.name
+        FROM auth_data
+        FULL JOIN user_data on user_data.user_id = auth_data.user_id
+        WHERE login=$1`, 
+        [user_login]
+    );
+    if (rows.length) {
+        return rows[0];
+    }
+    return null;
 }
 
 
 
-export async function createNewUser(bd: PostgresDb, user_data: RegistrationModel) {
+export async function createNewUser(bd: PostgresDb, user_data: RegistrationModel): Promise<string> {
     return await bd.transact(async bd => {
         await bd.query('INSERT INTO auth_data (login, password, user_id) VALUES ($1, $2, $3) RETURNING id', [user_data.login, user_data.password, user_data.user_id])
         await bd.query('INSERT INTO user_data (name, user_id) VALUES ($1, $2) RETURNING id', [user_data.name, user_data.user_id])
         return user_data.user_id
-    })
+    });
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { RegistrationRequest, CheckLoginRequest, LoginRequest } from '../../type
 import { FastifyInstance } from 'fastify';
 
 import { validateLength } from './helpers';
-import { getUserToLogin, createNewUser } from './auth.repositories';
+import { checkUseLogin, createNewUser, getUserData } from './auth.repositories';
 const bcrypt = require('bcrypt');
 
 class RegistrationService {
@@ -18,30 +18,46 @@ class RegistrationService {
                 errorMessage: 'Некорректные данные' 
             })
         }
-        const user = await getUserToLogin(fastify.pg, login)
+        const user = await checkUseLogin(fastify.pg, login)
 
         if (user) {
             return reply.code(400).send({ 
                 error: true, 
-                errorMessage: 'Такой пользователь уже есть' 
+                errorMessage: `Пользователь ${login} уже существует` 
             })
         }
         const hashPassword = await bcrypt.hash(password, 10)
         const user_id = uuidv4()
+        try {
+            await createNewUser(fastify.pg, { login, password: hashPassword, name, user_id })
+            
+            return reply.code(201).send({ user_id });
 
-        await createNewUser(fastify.pg, { login, password: hashPassword, name, user_id })
-        return reply.code(201).send({ user_id });
+        } catch(e) {
+            return reply.code(500).send({ 
+                error: true, 
+                errorMessage: `Произошла ошибка. Попробуйте позже.` 
+            })
+        }
+        
+        
     }
 
     async checkLogin(fastify: FastifyInstance, req: CheckLoginRequest, reply: FastifyReply) {
         const { login } = req.body;
         
-        const user = await getUserToLogin(fastify.pg, login)
+        if (!validateLength([[login, 5]])) {
+            return reply.code(400).send({ 
+                error: true, 
+                errorMessage: 'Некорректные данные' 
+            })
+        }
 
+        const user = await checkUseLogin(fastify.pg, login);
         if (user) {
             return reply.code(400).send({ 
                 error: true, 
-                errorMessage: 'Такой пользователь уже есть' 
+                errorMessage: `Пользователь ${login} уже существует` 
             })
         }
         return reply.code(200).send({
@@ -53,36 +69,37 @@ class RegistrationService {
 
         const { login, password } = req.body;
 
-        if (!validateLength([[login, 5], [password, 5])) {
+        if (!validateLength([[login, 5], [password, 5]])) {
             return reply.code(400).send({ 
                 error: true, 
                 errorMessage: 'Некорректные данные' 
             })
         }
 
-        const user = await getUserToLogin(fastify.pg, login)
+        const user = await getUserData(fastify.pg, login);
 
         if (!user) {
             return reply.code(400).send({ 
                 error: true, 
                 errorMessage: 'Пользователя не существует' 
             })
+        };
+
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return reply.code(400).send({ 
+                error: true, 
+                errorMessage: 'Неверная связка логин/пароль' 
+            })
         }
 
-        const validPassword = bcrypt.compare(myPlaintextPassword, user.password)
-
-
-
         return reply.code(200).send({
-            result: true
+            login: user.login,
+            name: user.name,
+            user_id: user.user_id
         });
 
     }
-
-    
-
-    
-
     
 }
 
